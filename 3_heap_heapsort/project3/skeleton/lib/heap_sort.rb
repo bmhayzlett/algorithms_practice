85988ff9d9019ffc356a65e40544bd0e410dfcbf
Brian Hayzlett <brianhayzlett@yahoo.com>
2016-07-31 14:51:20 -0700

    complete heapsort

--- 3_heap_heapsort/project3/skeleton/lib/heap_sort.rb
+++ 3_heap_heapsort/project3/skeleton/lib/heap_sort.rb
@@ -2,5 +2,21 @@ require_relative "heap"
 
 class Array
   def heap_sort!
+    iterator = 1
+
+    while iterator < self.length
+      BinaryMinHeap.heapify_up(self, iterator, self.length)
+      iterator += 1
+    end
+
+    iterator = self.length - 1
+
+    while iterator > 0
+      self[0], self[iterator] = self[iterator], self[0]
+      BinaryMinHeap.heapify_down(self,0,iterator)
+      iterator -= 1
+    end
+
+    self.reverse!
   end
 end
